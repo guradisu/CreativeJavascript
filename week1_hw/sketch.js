c4925b72b11570c11aff04526896d58696d0eec3
Gladys Chan <gladys.ly.chan@gmail.com>
2014-04-01 06:10:57 -0400

    week 1 hw

--- week1_hw/sketch.js
+++ week1_hw/sketch.js
@@ -1,3 +1,12 @@
+
+
+//declare pin
+var gridSize = 20;
+var happiness = 255;
+var pinSize = 14;
+var num = 500/gridSize;
+var pins = createArray(num,num);
+
 function createArray(length) {
     var arr = new Array(length || 0),
         i = length;
@@ -10,44 +19,40 @@ function createArray(length) {
     return arr;
 }
 
-static final int gridSize = 20;
-var num = 500/gridSize;
-var pins = createArray(num,num);
-
 function setup() {
   createCanvas(500,500);
   smooth();
-  for (i=0; i < pins.length; i++)  {
-    for (j=0; j < pins.length; j++) {
+  print(pins.length);
+  for (var i=0; i < pins.length; i++)  {
+    for (var j=0; j < pins.length; j++) {
       pins[i][j] = new Pin(i*gridSize, j*gridSize);
     }
   }
 }
 
 function draw() {
-  background(255);
-  for (i=0; i < pins.length; i++) {
-    for (j=0; j < pins.length; j++) {
+  background(happiness-10,happiness-10,happiness-10);
+  for (var i=0; i < pins.length; i++) {
+    for (var j=0; j < pins.length; j++) {
       pins[i][j].update();
       pins[i][j].display();
     }
   }
 }
 
-function Pin (x,y) {
+function Pin(x,y) {
     this.pinX = x;
     this.pinY = y;
     this.dist = 99999.0;
-}
-  
-Pin.prototype.update = function() {
-    this.dist = sqrt(sq(this.pinX - mouseX) + sq(this.pinY - mouseY));
-}
+};
 
-Pin.prototype.display = function()) {
+Pin.prototype.update = function() {
+  dist = sqrt(sq(this.pinX - mouseX) + sq(this.pinY - mouseY));
+};
+
+Pin.prototype.display = function() {
     var theta = PI/2*exp(-0.08*dist);
-    stroke(255);
+    stroke(dist*7,dist*3,dist*4);
     line(this.pinX+sin(theta)*pinSize/2, this.pinY+(1-cos(theta))*pinSize/2, 
         this.pinX-sin(theta)*pinSize/2, this.pinY+(1+cos(theta))*pinSize/2);    
-  }
-}
+};
